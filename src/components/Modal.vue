20ddafaf84fd476da5ff7c3189cd090ba794addd
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-02-01 07:47:01 +0100

    added modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,178 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            <p>Filtre par classe</p>
+            <select v-model="selectedType" @change="filterGroups">
+              <option
+                v-for="(group, index) in uniqueTypes"
+                :value="group.type"
+                :key="index"
+              >
+                {{ group.type }}
+              </option>
+            </select>
+          </slot>
+        </header>
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            <div>
+              <p>Filtre par nombre minimum d'utilisateurs</p>
+              <input
+                v-model="usersNumber"
+                @change="filterGroups"
+                type="number"
+                min="0"
+                placeholder="Rentrez un nombre"
+              />
+            </div>
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <div>
+              <p>Filtre par nom de groupe</p>
+              <input
+                v-model="userName"
+                @change="filterGroups"
+                type="text"
+                placeholder="Rentrez un nom de groupe"
+              />
+            </div>
+          </slot>
+        </footer>
+        <button
+          type="button"
+          class="btn-green"
+          @click="close"
+          aria-label="Close modal"
+        >
+          Use selected filter
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  props: ['groups'],
+  data() {
+    return {
+      filteredGroups: this.$props.groups,
+      uniqueTypes: [],
+      selectedType: {},
+      usersNumber: 0,
+      userName: '',
+    };
+  },
+  methods: {
+    getUniqueTypes() {
+      this.uniqueTypes = this.$props.groups
+        .map((e) => e['type'])
+        .map((e, i, final) => final.indexOf(e) === i && i)
+        .filter((obj) => this.$props.groups[obj])
+        .map((e) => this.$props.groups[e]);
+    },
+    filterGroups() {
+      if (Object.entries(this.selectedType).length > 0) {
+        this.filteredGroups = this.$props.groups.filter(
+          (group) => group.type === this.selectedType,
+        );
+      }
+      if (this.userName) {
+        this.filteredGroups = this.$props.groups.filter((group) =>
+          group.name.toLowerCase().includes(this.userName.toLowerCase()),
+        );
+      }
+      if (this.usersNumber) {
+        this.filteredGroups = this.$props.groups.filter(
+          (group) => group.users.length >= this.usersNumber,
+        );
+      }
+      return this.filteredGroups;
+    },
+    resetFilters() {
+      this.selectedType = {};
+      this.usersNumber = null;
+      this.userName = '';
+    },
+    close() {
+      this.$emit('onFilter', this.filteredGroups);
+      this.resetFilters();
+    },
+  },
+  mounted() {
+    this.getUniqueTypes();
+  },
+};
+</script>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  border-bottom: 1px solid #eeeeee;
+  color: #4aae9b;
+  justify-content: space-between;
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.btn-close {
+  border: none;
+  font-size: 20px;
+  padding: 20px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+</style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="test">
-    <ul v-for="(group, index) in filterGroups" v-bind:key="index">
+    <ul v-for="(group, index) in array" v-bind:key="index">
       <li>
         <div class="card">
           <h1>{{ group.name }}</h1>
@@ -10,96 +10,44 @@
       </li>
     </ul>
     <div>
-      <button @click="toggleFilter" type="button" :disabled="isFilterActivated">
+      <button @click="toggleFilter" type="button">
         Filter
       </button>
       <button @click="resetData" type="button">Clear</button>
     </div>
-    <div v-if="isFilterActivated">
-      <p>Filtre par classe</p>
-      <select v-model="selectedType">
-        <option
-          v-for="(group, index) in uniqueTypes"
-          :value="group.type"
-          :key="index"
-        >
-          {{ group.type }}
-        </option>
-      </select>
-      <div>
-        <p>Filtre par nombre minimum d'utilisateurs</p>
-        <input
-          v-model="usersNumber"
-          type="number"
-          min="0"
-          placeholder="Rentrez un nombre"
-        />
-      </div>
-      <div>
-        <p>Filtre par nom de groupe</p>
-        <input
-          v-model="userName"
-          type="text"
-          placeholder="Rentrez un nom de groupe"
-        />
-      </div>
-    </div>
+    <modal
+      v-show="isFilterModalVisible"
+      :groups="array"
+      @onFilter="changeHandler"
+      @close="toggleFilter"
+    />
   </div>
 </template>
-
 <script>
 import groups from '../data/groups.json';
+import modal from './Modal.vue';
 export default {
   name: 'Test',
-  computed: {
-    filterGroups() {
-      let filteredGroups = groups;
-      /* eslint-disable no-console */
-      if (Object.entries(this.selectedType).length > 0) {
-        filteredGroups = groups.filter((group) => group.type === this.selectedType);
-      }
-      if (this.userName) {
-        filteredGroups = groups.filter((group) =>
-          group.name.toLowerCase().includes(this.userName.toLowerCase()),
-        );
-      }
-      if (this.usersNumber) {
-        filteredGroups = groups.filter(
-          (group) => group.users.length >= this.usersNumber,
-        );
-      }
-      return filteredGroups;
-    },
+  components: {
+    modal,
   },
   data() {
     return {
-      isFilterActivated: false,
-      uniqueTypes: [],
-      selectedType: {},
-      usersNumber: null,
-      userName: '',
+      array: groups,
+      isFilterModalVisible: false,
     };
   },
   methods: {
-    getUniqueTypes() {
-      this.uniqueTypes = groups
-        .map((e) => e['type'])
-        .map((e, i, final) => final.indexOf(e) === i && i)
-        .filter((obj) => groups[obj])
-        .map((e) => groups[e]);
-    },
     toggleFilter() {
-      this.isFilterActivated = !this.isFilterActivated;
+      this.isFilterModalVisible = !this.isFilterModalVisible;
     },
     resetData() {
-      this.selectedType = {}, 
-      this.usersNumber = null;
-      this.userName = '';
-      this.toggleFilter();
+      this.array = groups;
     },
-  },
-  mounted() {
-    this.getUniqueTypes();
+    changeHandler(value) {
+      this.array = value;
+      this.isFilterModalVisible = false;
+    },
   },
 };
 </script>
